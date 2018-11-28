b95e1a8f733ed7057d1de22734310b924bb96e5b
Rezekiel <Rezekiel@github.com>
2018-11-27 18:24:47 -0600

    testing map display

--- java/map.js
+++ java/map.js
@@ -1,7 +1,24 @@
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("map"),mapProp);
+function initMap(){
+  var el = document.getElementById('map');
+  var iit = new google.maps.LatLng(41.8341216635,-87.623664172);
+  var crete = new google.maps.LatLng(41.444481,-87.631432);
+//map setting parameters
+  var mapOptions = {
+    center: new google.maps.LatLng(41.6572562,-87.6800486),
+    zoom: 15,
+  };
+  var myMap = new google.maps.Map(el, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: iit,
+    map: myMap,
+    animation:google.maps.Animation.BOUNCE,
+  });
+
+  var marker1 = new google.maps.Marker({
+    position: crete,
+    map: myMap,
+    animation:google.maps.Animation.BOUNCE,
+  });
+
 }
