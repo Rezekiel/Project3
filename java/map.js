e53b791503e25a7b04edeaec7c017f9debe2f478
Rezekiel <Rezekiel@github.com>
2018-11-27 21:42:03 -0600

    update long and lat for markers

--- java/map.js
+++ java/map.js
@@ -1,29 +1,82 @@
 function initMap() {
-       var iit = {lat:41.8350991, lng:-87.6276149};
-       var map = new google.maps.Map(document.getElementById('map'), {
-         zoom: 10,
-         center: iit
-       });
+  var map = {
+    lat: 41.8348731,
+    lng: -87.6270059,
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4.5,
+    center: map
+  });
 
-       var contentString = '<div id="content">'+
-           '<div id="siteNotice">'+
-           '</div>'+
-           '<h1 id="firstHeading" class="firstHeading">IIT</h1>'+
-           '<div id="bodyContent">'+
-           '<p><b>Wishnick Hall</b>ITMD 421: Data Modeling and Applications'+
-           '</div>'+
-           '</div>';
+  var wishnick = {
+    lat: 47.6062,
+    lng: -122.3321
+  };
+  var marker1 = new google.maps.Marker({
+    position: wishnick,
+    map: map,
+    title: "Wishnick Hall"
+  });
 
-       var infowindow = new google.maps.InfoWindow({
-         content: contentString
-       });
+  var rettaliata = {
+    lat: 41.8371064,
+    lng: -87.6271181
+  };
+  var marker2 = new google.maps.Marker({
+    position: rettaliata,
+    map: map,
+    title: "Rettaliata Center"
+  });
 
-       var marker = new google.maps.Marker({
-         position: iit,
-         map: map,
-         title: 'IIT'
-       });
-       marker.addListener('click', function() {
-         infowindow.open(map, marker);
-       });
-     }
+  var perlstein = {
+    lat: 44.4097,
+    lng: -103.5091
+  };
+  var marker3 = new google.maps.Marker({
+    position: perlstein,
+    map: map,
+    title: "Perlstein Hall"
+  });
+
+  var pritzker = {
+    lat: 41.8781,
+    lng: -87.6298
+  };
+  var marker4 = new google.maps.Marker({
+    position: pritzker,
+    map: map,
+    title: "Pritzker Hall"
+  });
+
+
+
+  var infowindow1 = new google.maps.InfoWindow({
+    content: "ITMD 421: Data Modeling and Applications"
+  });
+  marker1.addListener('click', function () {
+    infowindow1.open(map, marker1);
+  });
+
+  var infowindow2 = new google.maps.InfoWindow({
+    content: "PS-329: Environmental Politics/Policy"
+  });
+  marker2.addListener('click', function () {
+    infowindow2.open(map, marker2);
+  });
+
+  var infowindow3 = new google.maps.InfoWindow({
+    content: "ITM 100 Introduction to Information Technology as a Profession"
+  });
+  marker3.addListener('click', function () {
+    infowindow3.open(map, marker3);
+  });
+
+  var infowindow4 = new google.maps.InfoWindow({
+    content: "ITMD 361: Fall 2018 Fundamentals of Web Development"
+  });
+  marker4.addListener('click', function () {
+    infowindow4.open(map, marker4);
+  });
+
+ 
+}
