48dfcc4be92bfbe5dc91497ec9c386032d44f5c5
Rezekiel <Rezekiel@github.com>
2018-11-27 20:58:59 -0600

    java update

--- java/map.js
+++ java/map.js
@@ -1,22 +1,11 @@
 function initMap(){
-  var iit = document.getElementById('map');
-  var wishnick = new google.maps.LatLng(41.8350991,-87.6276149);
-  var  rettaliata = new google.maps.LatLng(41.8371064,-87.6271181)
-//map setting parameters
-  var mapOptions = {
-    center: new google.maps.LatLng(41.876518,-88.255531),
-    zoom: 12,
-  };
-  var map = new google.maps.Map(iit, mapOptions);
-
+  var location=[lat:41.876518,lng:-88.255531}
+  var map=new google.maps.Map(document.getElementById("map"),{
+    zoom:4,
+    center:location
+  });
   var marker = new google.maps.Marker({
-    position: wishnick,
-    map: map,
-    animation:google.maps.Animation.BOUNCE,
-  });
-
-  var marker1 = new google.maps.Marker({
-    position: rettaliata,
-    map: map,
-    animation:google.maps.Animation.BOUNCE,
-  });
+      position: location,
+      map:map
+  })
+}
