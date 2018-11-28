bac2afe9c7600cc7998628286be62a3098672dd8
Rezekiel <Rezekiel@github.com>
2018-11-27 20:50:14 -0600

    add new locations

--- java/map.js
+++ java/map.js
@@ -1,24 +1,22 @@
 function initMap(){
-  var el = document.getElementById('map');
-  var iit = new google.maps.LatLng(41.8341216635,-87.623664172);
-  var crete = new google.maps.LatLng(41.444481,-87.631432);
+  var iit = document.getElementById('map');
+  var wishnick = new google.maps.LatLng(41.8350991,-87.6276149);
+  var  rettaliata = new google.maps.LatLng(41.8371064,-87.6271181)
 //map setting parameters
   var mapOptions = {
-    center: new google.maps.LatLng(41.6572562,-87.6800486),
-    zoom: 15,
+    center: new google.maps.LatLng(41.876518,-88.255531),
+    zoom: 12,
   };
   var myMap = new google.maps.Map(el, mapOptions);
 
   var marker = new google.maps.Marker({
-    position: iit,
+    position: wishnick,
     map: myMap,
     animation:google.maps.Animation.BOUNCE,
   });
 
   var marker1 = new google.maps.Marker({
-    position: crete,
+    position: rettaliata,
     map: myMap,
     animation:google.maps.Animation.BOUNCE,
   });
-
-}
